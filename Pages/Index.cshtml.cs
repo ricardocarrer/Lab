71238e4f79bc6cf30952a3c9764d774d5b998bb6
Ricardo Carrer <ricardocarrer@gmail.com>
2020-06-25 13:07:46 -0300

    add arquivos

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Lab.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public void OnPostNome()
+        {
+            Nome = Request.Form["nome"].ToString();
+        }
+
+        public void OnPostSobrenome()
+        {
+            Sobrenome = Request.Form["nome"].ToString();
+        }
+    }
+}
